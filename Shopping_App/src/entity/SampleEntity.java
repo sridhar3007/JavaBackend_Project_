5de7e39f9301eb6434fb233807a78472c5a054fb
Sridhar Veeraiya <sridharveeraiya@gmail.com>
2025-07-30 11:28:15 +0530

    i have updated

--- Shopping_App/src/entity/SampleEntity.java
+++ Shopping_App/src/entity/SampleEntity.java
@@ -4,6 +4,7 @@ public class SampleEntity {
 
 	public static void main(String[] args) {
 		System.out.println("welcome...");
+		System.out.Println("uploading....");
 		// TODO Auto-generated method stub
 
 	}
